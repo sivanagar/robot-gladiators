db97c43e0fd176034317426ec08b6a27b3f4077e
Sivan Kaplan <sivanagar@gmail.com>
2021-08-11 18:26:49 -0700

    added getPlayerName function

--- assets/js/game.js
+++ assets/js/game.js
@@ -78,6 +78,7 @@ var startGame = function() {
     for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            debugger;
         var pickedEnemyObj = enemyInfo[i];
         pickedEnemyObj.health = randomNumber(40, 60);
         fight(pickedEnemyObj);
@@ -150,8 +151,20 @@ var shop = function() {
 }
 };  
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
